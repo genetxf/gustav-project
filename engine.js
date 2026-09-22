0184e7d8ef78312ba71813c3fd6381210ffa9084
Syed Foyez Uddin <foyezashik1013105@gmail.com>
2026-09-23 02:41:41 +0600

    Created and array of objects
    Determined and created the DOM in js.

---
+++ engine.js
@@ -0,0 +1,10 @@
+let guestList= [
+
+];
+
+const guestName = document.getElementById("guest-name");
+const guestPhone = document.getElementById("guest-phone");
+const guestClass = document.getElementById("guest-class");
+const guesAdd = document.getElementById("guest-add");
+
+
